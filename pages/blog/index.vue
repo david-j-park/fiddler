936c967e686d23212e6ccc5850eb1ce634471be4
David Park <djp22@columbia.edu>
2019-09-09 10:14:37 -0400

    adding slug

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,7 +2,7 @@
     <div class="bloglist">
         <h1>My Posts</h1>
         <ul>
-            <li v-for="p in blogPosts">{{p.title}}</li>
+            <li v-for="p in blogPosts">{{p.title}} {{p.slug}}</li>
         </ul>
     </div>
 </template>
